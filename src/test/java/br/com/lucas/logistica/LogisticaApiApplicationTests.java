3220181e8ce67c77535630a4326525ed2b19335f
Lucas Rodrigues Abreu <lucasrodriguesabreu@hotmail.com>
2021-05-17 16:08:07 -0300

    Subindo estrutura base do projeto

---
+++ src/test/java/br/com/lucas/logistica/LogisticaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucas.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
